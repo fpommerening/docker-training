812c92c2a794688553de65eea313a8a5c2859181
Frank Pommerening <frank@pommerening-online.de>
2019-04-20 21:53:43 +0200

    dotnet core: add secret sample

---
+++ dotnetcore/secrets/src/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using FP.DockerTraining.Secrets.Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FP.DockerTraining.Secrets.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Index()
+        {
+                var sb = new StringBuilder();
+                sb.AppendLine($"Secret-Sample {DateTime.Now:G}");
+                var databaseconfig = SecretHelper.GetSecret("databaseconfig.cfg");
+                if (string.IsNullOrEmpty(databaseconfig))
+                {
+                    sb.AppendLine("Es wurde keine databaseconfig gefunden");
+                }
+                else
+                {
+                    sb.AppendLine($"databaseconfig:{databaseconfig}");
+                }
+                return sb.ToString();
+
+        }
+    }
+}
