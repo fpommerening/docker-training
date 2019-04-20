a0a171e21c037afbd463ed8f07f43a104094f44f
Frank Pommerening <frank@pommerening-online.de>
2019-04-20 22:00:30 +0200

    dotnet core: Add samples config

---
+++ dotnetcore/configs/src/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace configs.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+      
+    }
+}
